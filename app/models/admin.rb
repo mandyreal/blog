3a42811234e72287d7dda2b2e0dff8248c98476c
Mandy Real <normandy.real@gmail.com>
2017-04-12 14:12:17 +0000

    Final app.

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,4 +3,6 @@ class Admin < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  #mount avatar uploader
+  mount_uploader :avatar, AvatarUploader
 end
