2b2ff42a65517d18997c00dc9715fcb705def330
Mandy Real <normandy.real@gmail.com>
2017-04-12 02:52:33 +0000

    Enabled Authentice action, added validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+    #Validate presence of title up to 140 chars
+    validates :title, presence: true, length: {maximum: 140}
+    #Validate presence of body
+    validates :body, presence: true
 end
